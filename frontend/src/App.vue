1a66919e8cf3e7b05ff2ffe22f34f0d135819ab4
natjef <natjef20@bergen.org>
2020-04-19 22:38:49 -0400

    Added json file, homepage content

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <Topbar/>
+    <HomepageContent/>
   </div>
 </template>
 
 <script>
 import Topbar from '@/components/Topbar.vue'
+import HomepageContent from '@/components/HomepageContent.vue'
+
 export default {
   name: "App",
   components: {
     Topbar,
+    HomepageContent,
   },
 };
 </script>
---
+++ frontend/src/components/HomepageContent.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="HomepageContent">
+        <!-- Header with image -->
+        <header>
+        <!-- Put an image or something here-->
+        </header>
+
+        <!-- About Container -->
+        <div class="container" id="about">
+            <div class="content" style="max-width:700px">
+                <h5 class="center padding-64"><span class="tag wide">ABOUT BANKCLASP</span></h5>
+                <p>We are a team of highschool students who are working on this project for a capstone class (expand).</p>
+                <p>The tool itself is designed to offer college players a rough estimate of (continue).</p>
+                <div class="panel leftbar light-grey">
+                    <p><i>designed for _________ (put the company name).</i></p>
+                    <p>Team members: Brandon Schein, Aidan Glickman, Anthony Alban, Nate Jeffries, Kris Pursianen</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- TOOL container -->
+        <div class="container" id="TOOL">
+            <div class="content" style="max-width:700px">
+
+                <h5 class="center padding-48"><span class="tag wide">PLAYER LOOKUP HERE</span></h5>
+
+                <div class="searchbar">
+                    <input type="text" placeholder="Search..">
+                </div>
+
+
+            </div>
+        </div>
+
+        <!-- Contact/Area Container -->
+        <div class="container" id="CONTACT US" style="padding-bottom:32px;">
+            <div class="content" style="max-width:700px">
+                <h5 class="center padding-48"><span class="tag wide">CONTACT US</span></h5>
+                <p>If you have any need to contact us.</p>
+                <p>Email us at: _________</p>
+                <p> or </p>
+                <p>Leave us an issue at (github link)</p>
+            </div>
+        </div>
+
+        <footer><i>Beta</i></footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomepageContent'
+    }
+</script>
+
+<style lang="css" scoped>
+</style>
