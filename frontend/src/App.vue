eaabe8d3139f75f2002539c17351d69cef548928
Aidan Glickman <aidan@aidanglickman.com>
2020-04-19 21:50:56 -0400

    submodules are dumb and should not exist to begin with

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Topbar/>
+  </div>
+</template>
+
+<script>
+import Topbar from '@/components/Topbar.vue'
+export default {
+  name: "App",
+  components: {
+    Topbar,
+  },
+};
+</script>
+
+<style lang="scss">
+    #app {
+        margin: 0 auto;
+        width: 1200px;
+    }
+</style>
