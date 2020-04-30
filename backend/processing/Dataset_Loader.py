c58b3eddccd36313c28f77c30184d527d5818702
Aidan Glickman <aidan@aidanglickman.com>
2020-04-30 10:40:34 -0400

    make dataset loader work properly and dump data

--- backend/processing/Dataset_Loader.py
+++ backend/processing/Dataset_Loader.py
@@ -2,64 +2,97 @@ import os
 import csv
 import pandas as pd
 import numpy as np
+import datetime
 
 
 class Dataset_Loader:
-    applicable_path = ""
-    players_path = ""
-    by_position = {}
+    datasets = {}
 
-    def load_dataset(self, position):
+    def load_dataset(self, position, drop_unlabeled=True):
         '''
-        returns a dataframe and list of labels for a given position to be used as a dataset
+        returns a dataframe for a given position to be used as a dataset
         '''
         position_data = self.by_position[position]
-        X_list, y = list(zip(*position_data))
-        X = pd.concat(X_list, axis=1).transpose()
-        return X, y
+        X = pd.concat(position_data, axis=0)
+        X.dropna(axis=0, how='any', inplace=True)
+        return X
 
     def aggregateStats(self, positionPlayer):
         '''
         sums all the stats to get down to one vector
         '''
-        return positionPlayer.sum(axis=0)
+        non_num = positionPlayer.select_dtypes(exclude=np.number)
+        num = positionPlayer.sum(axis=0, numeric_only=True).to_frame().T
+        return num.join(non_num)
 
     def loadPlayer(self, player_path):
         '''
-        loads a player in to the by_position dictionary
+        loads a player and returns a dict giving his aggregated stats by position
         '''
         player_dat = pd.read_csv(player_path)
         # get rid of the career row
         player_dat.drop(player_dat.index[-1], axis=0, inplace=True)
+        player_by_pos = {}
         for pos in player_dat['position'].unique():
             # Kickers and Punters provide errors, and people with no position aren't useful
             if pos in ["P", "K", "PK", "UT", np.nan]:
                 continue
             player_pos = player_dat.loc[player_dat['position'] == pos]
-            if pos not in self.by_position.keys():
-                self.by_position[pos] = []
-            # print(pos)
-            with open(os.path.join(self.applicable_path, pos + '.csv')) as applicable:
-                applicable_stats = list(
-                    map(lambda x: x.strip(), applicable.read().split(",")))
-                filtered_player = player_pos[applicable_stats]
-                self.by_position[pos].append(
-                    (self.aggregateStats(filtered_player), player_pos["player_id"].unique()[0]))
+            filtered_player = player_pos[self.applicables[pos] +
+                                         self.applicables['ALL']]
+            player_stats = self.aggregateStats(filtered_player)
+            # player_stats['id'] = player_pos["player_id"].unique()[0]
+            player_by_pos[pos] = player_stats
+        return player_by_pos
         # print(player_dat)
 
-    def loadAllPlayers(self):
+    def from_gathered(self, players_path):
         '''
-        iterates over all players to add them to by_position
+        iterates over all players 
         '''
-        for file in os.listdir(self.players_path):
-            self.loadPlayer(os.path.join(self.players_path, file, "ncaaf.csv"))
+        by_position = {}
+        for file in os.listdir(players_path):
+            player = self.loadPlayer(os.path.join(
+                players_path, file, "ncaaf.csv"))
+            for pos in player.keys():
+                if pos not in by_position.keys():
+                    by_position[pos] = []
+                by_position[pos].append(player[pos])
+        self.by_position = by_position
 
-    def __init__(self, applicable_path=os.path.join('applicable'), players_path=os.path.join('pairedPlayers')):
-        self.applicable_path = applicable_path
-        self.players_path = players_path
-        self.loadAllPlayers()
+    def from_file(self):
+        pass
+
+    def read_applicables(self, applicable_path):
+        applicables = {}
+        for filename in os.listdir(applicable_path):
+            with open(os.path.join(applicable_path, filename)) as applicable:
+                applicable_stats = list(
+                    map(lambda x: x.strip(), applicable.read().split(",")))
+                applicables[filename.split('.')[0]] = applicable_stats
+        return applicables
+
+    def dump_datasets(self, out_path):
+        os.makedirs(out_path, exist_ok=True)
+        print("TEST")
+        for position in self.by_position.keys():
+            print(position)
+            self.load_dataset(position).to_csv(
+                os.path.join(out_path, position + '.csv'))
+
+    def __init__(self, method='gathered', dump=False, applicable_path=os.path.join('applicable'), players_path=os.path.join('pairedPlayers'), dataset_path=os.path.join('datasets'), out_path=os.path.join('out', 'datasets', datetime.datetime.utcnow().strftime("%Y-%m-%d-%H:%M:%S"))):
+        self.method = method
+        if self.method == 'gathered':
+            self.applicables = self.read_applicables(applicable_path)
+            self.from_gathered(players_path)
+            if dump:
+                self.dump_datasets(out_path)
+        elif self.method == 'file':
+            self.dataset_path = dataset_path
+            self.from_file()
 
 
 if __name__ == "__main__":
-    loader = Dataset_Loader()
-    print(loader.load_dataset("QB")[0].info())
+    loader = Dataset_Loader(dump=True)
+    # print(loader.loadPlayer("pairedPlayers/AbduAm00/ncaaf.csv"))
+    print(loader.load_dataset("QB"))
--- backend/processing/ML_Algs.py
+++ backend/processing/ML_Algs.py
@@ -1,5 +1,15 @@
 import os
 from Dataset_Loader import Dataset_Loader
+from sklearn.metrics.pairwise import linear_kernel
+
+
+def lin_kern(base, comp):
+    sims = linear_kernel(base.drop("id", axis=1), comp.drop("id", axis=1))
+    return sims
+
 
 if __name__ == "__main__":
-    loader = Dataset_Loader(players_path=)
+    loader = Dataset_Loader()
+    print(loader.by_position["QB"][0].to_frame().transpose())
+    print(lin_kern(loader.load_dataset("QB"),
+                   loader.by_position["QB"][0].to_frame()))
