9024c2e0ec84e35f40841e74ca03562ec8585288
Aidan Glickman <aidan@aidanglickman.com>
2020-05-01 18:38:49 -0400

    write cosine similarity test

--- backend/processing/ML_Algs.py
+++ backend/processing/ML_Algs.py
@@ -1,15 +1,34 @@
 import os
 from Dataset_Loader import Dataset_Loader
-from sklearn.metrics.pairwise import linear_kernel
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 
-def lin_kern(base, comp):
-    sims = linear_kernel(base.drop("id", axis=1), comp.drop("id", axis=1))
-    return sims
+def cos_sim(dataset):
+    numeric = dataset.select_dtypes(
+        include=[np.number])
+    keys = dataset.select_dtypes(
+        exclude=[np.number])
+    scaler = StandardScaler()
+    scaled = scaler.fit_transform(numeric)
+    sims = cosine_similarity(scaled)
+    player_mappings = {}
+    for i in range(len(keys)):
+        analyzed = keys.iloc[i]
+        player_sims = sims[i]
+        player_df = keys
+        player_df['similarity'] = player_sims.tolist()
+        sorted_sims = player_df.sort_values(
+            by="similarity", axis=0, ascending=False)
+        player_mappings[analyzed.player_id] = sorted_sims
+    return player_mappings
+    # print(analyzed, sorted_sims)
+    # input()
+    # return sims
 
 
 if __name__ == "__main__":
-    loader = Dataset_Loader()
-    print(loader.by_position["QB"][0].to_frame().transpose())
-    print(lin_kern(loader.load_dataset("QB"),
-                   loader.by_position["QB"][0].to_frame()))
+    loader = Dataset_Loader(method='file')
+    dataset = loader.load_dataset("QB")
+    cos_sim(dataset)['lamar-jackson-1'].to_csv('jackson.csv')
