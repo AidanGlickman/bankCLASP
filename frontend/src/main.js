ea19bc1b08ceee38f26a9601fda237848208eefe
brandonschein <brandonschein11@gmail.com>
2020-05-14 22:52:34 -0400

    change bootstrap import

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
