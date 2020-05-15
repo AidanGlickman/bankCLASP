b95de3e804ffc48a277aff6e7fd0770d4abe643a
brandonschein <brandonschein11@gmail.com>
2020-05-15 13:31:15 -0400

    import node modules for dropdown

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,8 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Dropdown from 'vue-simple-search-dropdown'
+import "node_modules/vuejs/dist/vue.min.js"
+import "node_modules/vue-simple-search-dropdown/dist/vue-simple-search-dropdown.min.js"
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
