56f8ac5a960d9e7e2aba3cd4f0e26aa6d6fcb248
brandonschein <brandonschein11@gmail.com>
2020-05-15 00:02:07 -0400

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,11 +2,9 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/components/HomepageContent";
 import Tool from "@/components/Results";
-<<<<<<< HEAD
 import Player from "@/components/Player";
-=======
-import Contact from "@/components/Contact"
->>>>>>> d52083207487390d9f0b95ac69f67862e73559f8
+import Contact from "@/components/Contact";
+
 
 Vue.use(Router);
 
@@ -24,16 +22,15 @@ export default new Router({
       component: Tool,
     },
     {
-<<<<<<< HEAD
+
       path: "/player/:id",
       name: "player",
       component: Player,
     },
-=======
+    {
       path: "/contact",
       name: "contact",
       component: Contact,
-    }
->>>>>>> d52083207487390d9f0b95ac69f67862e73559f8
+    },
   ],
 });
