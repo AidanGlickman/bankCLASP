ba009a11b735c2b505c0769aa148ced719c72a63
brandonschein <brandonschein11@gmail.com>
2020-05-15 15:03:54 -0400

    add sportsreference link

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,12 @@ import router from "./router";
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Dropdown from 'vue-simple-search-dropdown'
 
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
+Vue.use(Dropdown)
 
 
 Vue.config.productionTip = false;
