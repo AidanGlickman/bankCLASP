409d19e3aa2f5c83481fd85657545eac5d053133
brandonschein <brandonschein11@gmail.com>
2020-05-15 13:37:27 -0400

    remove dropdown imports for now to make a branch instead

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,11 @@ import router from "./router";
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import Dropdown from 'vue-simple-search-dropdown'
-import "node_modules/vuejs/dist/vue.min.js"
-import "node_modules/vue-simple-search-dropdown/dist/vue-simple-search-dropdown.min.js"
+
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
-Vue.use(Dropdown)
+
 
 Vue.config.productionTip = false;
 
